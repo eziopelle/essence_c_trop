16e89b974d1c4c354354180f99faf53cae9be202
Victor Pelle <victor.pelle97@gmail.com>
2022-11-21 15:18:04 +0100

    feature new et create de car avec la vue

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,11 +1,19 @@
 class CarsController < ApplicationController
   def index
+    @cars = Cars.all
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(params_car)
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -18,5 +26,11 @@ class CarsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def params_car
+    params.require(:car).permit(:name, :photo)
   end
 end
