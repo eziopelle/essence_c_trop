22ef191078a0d7629ae3b1481438137358f4331e
Victor Pelle <victor.pelle97@gmail.com>
2022-11-21 15:27:04 +0100

    modif orthographe kilometreage

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -31,6 +31,6 @@ class CarsController < ApplicationController
   private
 
   def params_car
-    params.require(:car).permit(:name, :model, :color, :city, :price, :autonomy, :kiloetreage)
+    params.require(:car).permit(:name, :model, :color, :city, :price, :autonomy, :kilometreage)
   end
 end
