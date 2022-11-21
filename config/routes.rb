25045c68ad0d92f5154277e23a91f3982d670254
Victor Pelle <victor.pelle97@gmail.com>
2022-11-21 12:45:24 +0100

    ajout des 3 controllers et des methodes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/destroy'
+  get 'bookings/accept'
+  get 'bookings/decline'
+  get 'cars/index'
+  get 'cars/show'
+  get 'cars/new'
+  get 'cars/create'
+  get 'cars/edit'
+  get 'cars/destroy'
+  get 'cars/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
