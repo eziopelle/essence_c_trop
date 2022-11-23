0f088d4de23f215240604f8b6caec40fdb0bfd26
Victor Pelle <victor.pelle97@gmail.com>
2022-11-23 10:00:36 +0100

    préparation du code agenda en commentaire (TO DO)

--- app/javascript/controllers/flatpicker_controller.js
+++ app/javascript/controllers/flatpicker_controller.js
@@ -16,6 +16,11 @@ export default class extends Controller {
   }
 
 
+  //changeDate (event) {
+    //const value = event.currentTarget.value
+    //this.startDateTarget.value = value.split("to", 1);
+    //this.endDateTarget.value = value.split("to", [2])
+  }
 
 
 }
