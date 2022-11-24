6cb7cc6666b281f2bf03a98c183233349d2958e6
Victor Pelle <victor.pelle97@gmail.com>
2022-11-24 14:01:02 +0100

    ajout d'un titre au model review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_112334) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_125527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_112334) do
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["car_id"], name: "index_reviews_on_car_id"
   end
 
